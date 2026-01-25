dd835181dcd0fdbebe50b5b7e901e2118a61651e
kshiteejbalkavade11-crypto <kshiteejbalkavade11@gmail.com>
2026-01-25 13:33:23 +0530

    Homework.py

---
+++ Lesson 2/Homework.py
@@ -0,0 +1,15 @@
+Birthday_1= "11 November"
+Birthday_2= "15 December"
+Birthday_3= "22 November"
+Birthday_4= "18 January"
+Birthday_5= "21 July"
+Kshiteej= ("Kshiteej")
+Aryan= ("Aryan")
+Tanaya= ("Tanaya")
+Arnav= ("Arnav")
+Sidhesh= ("Sidhesh")
+print(Kshiteej,Birthday_1)
+print(Aryan,Birthday_2)
+print(Tanaya,Birthday_3)
+print(Arnav,Birthday_4)
+print(Sidhesh,Birthday_5)
